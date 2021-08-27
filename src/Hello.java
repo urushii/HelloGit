5bfd3836db51d2f7b536bee06e99510a48a785ca
calvi <calvi@DESKTOP-SL9VCQA.unl.edu>
2021-08-27 15:13:01 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Calvin Dao (cdao2@unl.edu) 
+ * Date: 08/27/2021
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author calvi
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
